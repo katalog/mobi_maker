909a31ef327ca730d24498e9469df4fc82c88ec9
eunkyoon kim <katalog@gmail.com>
2020-11-12 11:59:12 +0100

    Update mobi_maker.go
    support convert all files in dir

--- mobi_maker.go
+++ mobi_maker.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 )
 
-func main() {
-	//debug
-	//os.Args = []string{"kindmaker", "uoop.txt"}
-
-	if len(os.Args) != 2 {
-		fmt.Println("USAGE : kindleconv textfile.txt")
-		return
-	}
-
-	filenamewithext := os.Args[1]
+func convert(filenamewithext string) {
 	filename := strings.Replace(filenamewithext, ".txt", "", 1)
 
 	strResult := ReadTextFromFile(filenamewithext)
@@ -31,4 +24,38 @@ func main() {
 
 	// Output MOBI File
 	m.Write()
+}
+
+func main() {
+	//debug
+	//os.Args = []string{"kindmaker", "uoop.txt"}
+
+	if len(os.Args) != 2 {
+		fmt.Println("USAGE : mobi_maker textfile.txt")
+		fmt.Println("or      mobi_maker -a")
+		fmt.Println("-a option : convert all txt file in same folder")
+		return
+	}
+
+	lstFilename := []string{}
+
+	if os.Args[1] == "-a" {
+		files, err := ioutil.ReadDir("./")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, f := range files {
+			if strings.Contains(f.Name(), ".txt") {
+				fmt.Println(f.Name())
+				lstFilename = append(lstFilename, f.Name())
+			}
+		}
+	} else {
+		filenamewithext := os.Args[1]
+		lstFilename = append(lstFilename, filenamewithext)
+	}
+
+	for _, fname := range lstFilename {
+		convert(fname)
+	}
 }
